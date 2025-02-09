8200110ee8f7fa0319e10ed1492ebbb7f1639d7d
quanpmhe171342 <quanpmhe171342@fpt.edu.vn>
2025-02-09 13:32:58 +0700

    FIx

--- SWP391/src/java/Model/Color.java
+++ SWP391/src/java/Model/Color.java
@@ -9,5 +9,28 @@ package Model;
  * @author phuan
  */
 public class Color {
+    int Color_id;
+    String Color_Name;
+
+    public Color(int Color_id, String Color_Name) {
+        this.Color_id = Color_id;
+        this.Color_Name = Color_Name;
+    }
+
+    public int getColor_id() {
+        return Color_id;
+    }
+
+    public void setColor_id(int Color_id) {
+        this.Color_id = Color_id;
+    }
+
+    public String getColor_Name() {
+        return Color_Name;
+    }
+
+    public void setColor_Name(String Color_Name) {
+        this.Color_Name = Color_Name;
+    }
     
 }
