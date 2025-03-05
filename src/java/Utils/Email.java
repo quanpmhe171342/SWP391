1910502fc6c09d6fbd61dc350e73dd690f1cced2
HieuNMHE171532 <hieunmhe171532@fpt.edu.vn>
2025-03-05 11:23:39 +0700

    Update email

--- src/java/Utils/Email.java
+++ src/java/Utils/Email.java
@@ -1,13 +1,93 @@
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Utils;
 
+import java.util.Properties;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 /**
  *
  * @author hieum
  */
+//public class Email {
+////    hotelbookinghl@gmail.com
+////    hnmk sber whmi npfg
+//
+//    public static void main(String[] args) {
+//        final String from = "hotelbookinghl@gmail.com";
+//        final String password = "hnmksberwhminpfg";
+//        final String to = "hieuhieu1234231@gmail.com";
+//
+//        Properties props = new Properties();
+//        props.put("mail.smtp.host", "smtp.gmail.com"); // SMTP HOST
+//        props.put("mail.smtp.port", "587"); // TLS 587 SSL 465
+//        props.put("mail.smtp.auth", "true");
+//        props.put("mail.smtp.starttls.enable", "true");
+//    }
+//}
+
+
+
 public class Email {
-    
+
+    static final String from = "leatherswp391@gmail.com";
+    static final String password = "Leather1234";
+
+    public static boolean sendEmail(String to, String tieuDe, String noiDung) {
+        // Properties : khai báo các thuộc tính
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com"); // SMTP HOST
+        props.put("mail.smtp.port", "587"); // TLS 587 SSL 465
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        // create Authenticator
+        Authenticator auth = new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(from, password);
+            }
+        };
+
+        // Phiên làm việc
+        Session session = Session.getInstance(props, auth);
+
+        // Tạo một tin nhắn
+        MimeMessage msg = new MimeMessage(session);
+
+        try {
+            // Kiểu nội dung
+            msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
+
+            // Người gửi
+            msg.setFrom(from);
+
+            // Người nhận
+            msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to, false));
+
+            // Tiêu đề email
+            msg.setSubject(tieuDe);
+
+            // Nội dung
+            msg.setContent(noiDung, "text/HTML; charset=UTF-8");
+
+            // Gửi email
+            Transport.send(msg);
+            System.out.println("Gửi email thành công");
+            return true;
+        } catch (Exception e) {
+            System.out.println("Gặp lỗi trong quá trình gửi email");
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
