33ea4ef5fec4551359649eba5fe64f5161ffedcd
HieuNMHE171532 <hieunmhe171532@fpt.edu.vn>
2025-03-17 01:17:50 +0700

    Update send email

--- src/java/Utils/Email.java
+++ src/java/Utils/Email.java
@@ -19,28 +19,12 @@ import javax.mail.internet.MimeMessage;
  * @author hieum
  */
 //public class Email {
-////    hotelbookinghl@gmail.com
-////    hnmk sber whmi npfg
-//
-//    public static void main(String[] args) {
-//        final String from = "hotelbookinghl@gmail.com";
-//        final String password = "hnmksberwhminpfg";
-//        final String to = "hieuhieu1234231@gmail.com";
-//
-//        Properties props = new Properties();
-//        props.put("mail.smtp.host", "smtp.gmail.com"); // SMTP HOST
-//        props.put("mail.smtp.port", "587"); // TLS 587 SSL 465
-//        props.put("mail.smtp.auth", "true");
-//        props.put("mail.smtp.starttls.enable", "true");
-//    }
-//}
-
-
-
+////    hieuminh21102003@gmail.com
+////    njfuwkiyvrvhqxzn
 public class Email {
 
-    static final String from = "leatherswp391@gmail.com";
-    static final String password = "Leather1234";
+    static final String from = "hieuminh21102003@gmail.com";
+    static final String password = "njfuwkiyvrvhqxzn";
 
     public static boolean sendEmail(String to, String tieuDe, String noiDung) {
         // Properties : khai báo các thuộc tính
@@ -88,6 +72,56 @@ public class Email {
             System.out.println("Gặp lỗi trong quá trình gửi email");
             e.printStackTrace();
             return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        final String from = "hieuminh21102003@gmail.com";
+        final String password = "njfuwkiyvrvhqxzn";
+        
+        final String to = "hieuhieu1234231@gmail.com";
+        
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com"); // SMTP HOST
+        props.put("mail.smtp.port", "587"); // TLS 587 SSL 465
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        
+        Authenticator auth = new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(from, password);
+            }
+        };
+        
+        Session session = Session.getInstance(props, auth);
+        
+        MimeMessage msg = new MimeMessage(session);
+        
+        try {
+            // Kiểu nội dung
+            msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
+
+            // Người gửi
+            msg.setFrom(from);
+
+            // Người nhận
+            msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to, false));
+
+            // Tiêu đề email
+            msg.setSubject("Test send email");
+
+            // Nội dung
+            msg.setContent("This is a test", "text/HTML; charset=UTF-8");
+
+            // Gửi email
+            Transport.send(msg);
+            System.out.println("Gửi mail thành công");
+            
+        } catch (Exception e) {
+            System.out.println("Gặp lỗi trong quá trình gửi email");
+            e.printStackTrace();
+            
         }
     }
 }
